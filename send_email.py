c26db27a3e571dcf6377f67000277a9641b6304d
goaliejordan <goaliejordan@gmail.com>
2017-11-30 13:28:23 -0700

    added email script for automating emails

---
+++ send_email.py
@@ -0,0 +1,28 @@
+import smtplib
+
+gmail_user = 'jordansmitty01@gmail.com'
+gmail_password = '5t0r@g3!'
+
+sent_from = 'jordansmitty01@gmail.com'
+to = ['goaliejordan@gmail.com']
+subject = 'OMG Super Important Message'
+body = "Hey, what's up?\n\n- You"
+
+email_text = """\  
+From: %s  
+To: %s  
+Subject: %s
+
+%s
+""" % (sent_from, ", ".join(to), subject, body)
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, email_text)
+
+    print 'email sent!'
+except:
+    print 'Something went wrong...'
+
