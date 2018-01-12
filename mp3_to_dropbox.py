99aafb422a89ccccc223ecb2cdeaf5b9fbdec57d
goaliejordan <goaliejordan@gmail.com>
2018-01-12 16:36:19 -0700

    Corrected variables for local copy of mp3 in upload function.

--- mp3_to_dropbox.py
+++ mp3_to_dropbox.py
@@ -1,50 +1,76 @@
-import dropbox
+import os
+import sys
+import requests
 import urllib
-import urllib2
-from datetime import date
+import time
+from datetime import date, datetime, timedelta
+
+import myconfig
+import dropbox
 from dropbox.files import WriteMode
 from dropbox.exceptions import ApiError, AuthError
-import sys
+
 
 # dropbox variables
-ap_key = "rqgmlp4uyg6pgqe"
-ap_secret = "rqgmlp4uyg6pgqe"
-db_token = "qW1XPtvlNYwAAAAAAADHrzd2EiRF1jsDJxKPs47z1CBiKaEEQ2S3e-meP_UWxK2-"
+db_token = myconfig.db_token
 
 # mp3 variables
+present = datetime.now()
+thirty_days_ago = datetime.now() - timedelta(days=30)
 today = str(date.today()).replace("-", "")
 file_location = "c:\\temp\\"
-file_name = "TheBriefing%s.mp3" % today
-local_copy_mp3 = file_location + file_name
+file_name = "TheBriefing{}.mp3".format(today)
+local_copy_mp3 = os.path.join(file_location, file_name)
 dropbox_folder = "/TheBriefing"
-upload_path = (dropbox_folder + "/" + file_name)
+upload_path = os.path.join(dropbox_folder, file_name)
 
 # get the url with the date.
-mp3_url = "https://mohler-media-5ox2mshyj.stackpathdns.com/Podcast/%s_TheBriefing.mp3" % today
+mp3_url = "https://mohler-media-5ox2mshyj.stackpathdns.com/Podcast/{}_TheBriefing.mp3".format(today)
 
 
 def ensure_url_exists(url):
-    try:
-        urllib2.urlopen(url)
-    except urllib2.HTTPError, e:
-        return e.code, "The Briefing does not exist for that date."
-    except urllib2.URLError, e:
-        return e.args, "Unable to retrieve The Briefing MP3."
+    request = requests.get(url)
+    if request.status_code != 200:
+        return request.status_code, "The Briefing does not exist for that date."
 
 
 def download_mp3():
     urllib.urlretrieve(mp3_url, local_copy_mp3)
 
-# Uploads The Briefing MP3 to Dropbox
+
+def remove_month_old_db_mp3():
+    # Remove mp3s that are older than 30 days from dropbox.
+    thirty_days_ago = datetime.now() - timedelta(days=30)
+    entries = dbx.files_list_folder(dropbox_folder)
+    for entry in entries.entries:
+        file_creation_time = entry.server_modified.date()
+        # delete the file
+        if file_creation_time < thirty_days_ago:
+            print "Deleting dropbox file {}".format(entry.name)
+            dbx.files_delete_v2((dropbox_folder + "/" + entry.name))
 
 
+
+
+def remove_month_old_os_mp3():
+    # Remove mp3s that are older than 30 days from dropbox.
+    # get files in OS and check thier time:
+    #    for files in os location:
+    # filecreated = os.path.getmtime("c:\\temp\\TheBriefing20180103.mp3")
+    #       time.ctime(filecreated)
+    #    #       if filecreated < 30_days_ago
+    #       delete file
+    #
+    # filetime = datetime.datetime.fromtimestamp(filecreated)
 def mp3_upload():
+    # Uploads The Briefing MP3 to Dropbox
+    dropbox_upload_path = str(upload_path).replace('\\', '/')
     with open(local_copy_mp3, 'rb') as f:
         # We use WriteMode=overwrite to make sure that the settings in the file
         # are changed on upload
-        print("Uploading " + file_name + " to Dropbox as " + upload_path + "...")
+        print("Uploading " + file_name + " to Dropbox as " + dropbox_upload_path + "...")
         try:
-            dbx.files_upload(f.read(), upload_path, mode=WriteMode('overwrite'))
+            dbx.files_upload(f.read(), dropbox_upload_path, mode= WriteMode('overwrite'))
         except ApiError as err:
             # This checks for the specific error where a user doesn't have
             # enough Dropbox space quota to upload this file
@@ -52,20 +78,18 @@ def mp3_upload():
                     err.error.get_path().reason.is_insufficient_space()):
                 sys.exit("ERROR: Cannot back up; insufficient space.")
             elif err.user_message_text:
-                print(err.user_message_text)
+                print err.user_message_text
                 sys.exit()
             else:
-                print(err)
+                print err
                 sys.exit()
-
-# connect to dropbox account
 
 
 if __name__ == '__main__':
     # Check for an access token
     if len(db_token) == 0:
         sys.exit("ERROR: Looks like you didn't add your access token. "
-            "You will need to get this token from dropbox.com.")
+                 "You will need to get this token from dropbox.com.")
 
     # Create an instance of a Dropbox class, which can make requests to the API.
     print("Creating a Dropbox object...")
@@ -89,4 +113,8 @@ if __name__ == '__main__':
 
     # Remove 30 day old files from dropbox
 
-    print("Done!")
+    print "Done!"
+
+
+help = 'https://discourse.mcneel.com/t/python-variables-and-arguments-best-practices/31440/3'
+
